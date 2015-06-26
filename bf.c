b9b7d519f407c6237162666041fb2442b4220029
tomsik68 <tomsik68@gmail.com>
2015-06-26 21:46:08 +0000

    Loading command line arguments

--- bf.c
+++ bf.c
@@ -10,7 +10,11 @@
  * 	-o	--output	false		At the end of program execution, the character array will be dumped to stdout
  */
 
+
 #include <stdio.h>
+#include <string.h>
+
+
 // Brainfuck interpreter configuration
 typedef struct bf_config_t {
 	// length of main character array
@@ -23,8 +27,39 @@ typedef struct bf_config_t {
 	int 	read_stdout;
 } bf_config_t;
 
+// creates the default configuration
+void configure_default(bf_config_t* config){
+	config->array_length 	= 65536;
+	config->dump_memory 	= 0;
+	config->filename 	= "-";
+}
+
+// this macro evaluates to true if all characters in s1 and s2 are equal
+#define STRINGS_EQUAL(s1,s2) (strcmp(s1,s2) == 0)
+
+/**
+ * Creates configuration for brainfuck interpreter based on values of arguments
+ */
+bf_config_t* configure(int argc, char** argv){
+	bf_config_t* result = malloc(sizeof(bf_config_t));
+	configure_default(result);
+	// we're iterating up to argc - 2, because argc - 1 stores the file name
+	for(int i = 0; i < argc - 1; ++i){
+		if(STRINGS_EQUAL(argv[i], "--memory") || STRINGS_EQUAL(argv[i], "-m")){
+			// get memory amount from argv[i+1]
+			i += 1;
+		} else if(STRINGS_EQUAL(argv[i], "-o") || STRINGS_EQUAL(argv[i], "--output")){
+			result->dump_memory = 1;
+		}
+	}
+	// load filename from argc-1
+	result->filename = argv[argc-1];
+	return result;
+}
+
 int main(int argc, char** argv){
 	// load command line arguments
+	bf_config_t* config = configure(argc, argv);
 	// create bf execution environment based on parameters
 	// load program
 	// execute program
