47bd93c27f30d21170b9b9379d3dbead2de30306
tomsik68 <tomsik68@gmail.com>
2015-06-26 21:02:03 +0000

    Added descriptive comments

--- bf.c
+++ bf.c
@@ -1,6 +1,24 @@
+/*
+ * Brainfuck.c - A brainfuck language interpreter.
+ *
+ * Usage:
+ * 	bfi <filename.bf> [options]
+ *
+ * Options:
+ * 	Short	Long		Default value	Description
+ * 	-m	--memory	65536		Sets length of character array
+ * 	-o	--output	false		At the end of program execution, the character array will be dumped to stdout
+ */
+
 #include <stdio.h>
 
 int main(int argc, char** argv){
+	// load command line arguments
+	// create bf execution environment based on parameters
+	// load program
+	// execute program
+	// dump memory if asked to
+	// cleanup
 	return 0;
 }
 
