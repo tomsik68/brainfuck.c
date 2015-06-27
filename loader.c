eff72964aec63924cb6fda6efbd4290702d1403c
tomsik68 <tomsik68@gmail.com>
2015-06-27 08:37:08 +0000

    Finished loading and linking while loops

--- loader.c
+++ loader.c
@@ -1,5 +1,17 @@
 #include "loader.h"
 
+/*
+ * This is stack used for while loops in the program.
+ * When loader finds a WHILE instruction, it will push it to a stack.
+ * When it finds ENDWHILE, it will pop address to jump back to from the stack.
+ */
+typedef struct loopstart_t {
+	/* previous brackets_t in the stack */
+	struct loopstart_t*	prev;
+	/* address of this bracket */
+	int			address;
+} loopstart_t;
+
 /* returns 1 if this character is a valid instruction character */
 int is_instruction(char c){
 	static char* instructionChars = "+-[],.<>";
@@ -40,14 +52,20 @@ bf_instruction_code_t type_from_char(char c){
 	return result;
 }
 
+
+
+
 /* we won't read the whole program at once. instead, we'll use blocks to read it... */
 #define BLOCK_SIZE 128
 /* loads program from a stream */
-bf_instruction_t* load_program_from_stream(FILE* stream){ bf_instruction_t* result = malloc(sizeof(bf_instruction_t));
+bf_instruction_t* load_program_from_stream(FILE* stream){
+	bf_instruction_t* result = malloc(sizeof(bf_instruction_t));
 	result->prev = NULL;
 	bf_instruction_t* current = result;
+	loopstart_t* loopstarts = NULL;
 	/* block of program */
 	char block[BLOCK_SIZE];
+	int address = 0;
 	while(!feof(stream)){
 		size_t bytesRead = fread(block, sizeof(char), BLOCK_SIZE, stream);
 		/* process the block */
@@ -60,6 +78,28 @@ bf_instruction_t* load_program_from_stream(FILE* stream){ bf_instruction_t* resu
 				current->next = malloc(sizeof(bf_instruction_t));
 				current->next->prev = current;
 				current->parameters = 0;
+				/* perform while loop linking */
+				if(current->type == WHILE){
+					/* PUSH address */
+					loopstart_t* newloop = malloc(sizeof(loopstart_t));
+					newloop->prev = loopstarts;
+					newloop->address = address;
+					loopstarts = newloop;
+				} else if(current->type == ENDWHILE){
+					/* POP address */
+					loopstart_t* loop = loopstarts;
+					if(loop == NULL) {
+						/* TODO: ERROR: unbalanced loops */
+					}
+					loopstarts = loopstarts->prev;
+					/* save the address in parameters */
+					current->parameters = malloc(sizeof(int));
+					memcpy(current->parameters, &(loop->address), sizeof(int));
+					free(loop);
+				}
+				/* move the pointer */
+				current = current->next;
+				++address;
 			}
 		}
 	}
