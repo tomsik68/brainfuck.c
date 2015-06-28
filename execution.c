6c00c43ae2e652ee3ca5291f87c3db31ee172d4a
tomsik68 <tomsik68@gmail.com>
2015-06-28 13:39:03 +0000

    More efficient array initializer

--- execution.c
+++ execution.c
@@ -5,10 +5,7 @@ bf_env_t* create_environment(bf_config_t* config){
 	/* allocate memory for execution */
 	result->memory = malloc(config->array_length * sizeof(char));
 	/* initialize memory to all zeros */
-	int i;
-	for( i = 0; i < config->array_length; ++i){
-		result->memory[i] = 0;
-	}
+	memset(result->memory, 0, config->array_length);
 	/* the pointer points to the first element of memory array */
 	result->ptr = 0;
 	result->address = 0;
